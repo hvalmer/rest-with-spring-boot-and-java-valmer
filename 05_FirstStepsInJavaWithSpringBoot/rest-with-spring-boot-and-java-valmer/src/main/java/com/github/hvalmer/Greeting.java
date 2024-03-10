e4b40d09d567e80c815da92c32a7118fd1662b90
hvalmer <hvalmer@gmail.com>
2024-03-10 11:01:46 -0300

    Fist steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-valmer/src/main/java/com/github/hvalmer/Greeting.java
@@ -0,0 +1,21 @@
+package com.github.hvalmer;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+	
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+}
